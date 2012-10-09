65919b6cc80b64b82f7a0a28380140b42db3f50f
Darren Jeacocke <dazonic@me.com>
2012-10-09 18:42:00 +1000

    rails aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,7 +1,5 @@
-alias r='rbenv local 1.8.7-p358'
-
-alias sc='script/console'
-alias sg='script/generate'
-alias sd='script/destroy'
+alias rc='rails console'
+alias rg='rails generate'
+alias rd='rails destroy'
 
 alias migrate='rake db:migrate db:test:clone'
