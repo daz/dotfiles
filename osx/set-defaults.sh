2b8b83db0d36655d2c7f832daa8a596606ec0571
Darren Jeacocke <dazonic@me.com>
2014-08-30 18:21:12 +1000

    remove a few unwanted defaults

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -1,9 +1,6 @@
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-# Use AirDrop over every interface. srsly this should be a default.
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
-
 # Always open everything in Finder's list view.
 defaults write com.apple.Finder FXPreferredViewStyle clmv
 
@@ -33,17 +30,3 @@ defaults write com.apple.universalaccess useMouseKeysShortcutKeys -bool false
 # Sticky keys
 defaults write com.apple.universalaccess stickyKey -bool true
 defaults write com.apple.universalaccess stickyKeyBeepOnModifier -bool false
-
-# Set the Finder prefs for showing a few different volumes on the Desktop.
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-
-# Hide Safari's bookmark bar.
-defaults write com.apple.Safari ShowFavoritesBar -bool false
-
-# Set up Safari for development.
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
